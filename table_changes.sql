a205950ac9247531a65794bda36d961b4a8a3857
Sharath B Naik <appyesme@gmail.com>
2025-04-07 22:17:52 +0530

    sync

--- table_changes.sql
+++ table_changes.sql
@@ -1,8 +1,3 @@
 begin;
 
-
-alter table payments add column refund_id varchar(255);
-UPDATE payments SET status = 'SETTLED' WHERE status = 'CLEARED'; -- Check this app side also
--- Add webhook refund response
-
 commit;
